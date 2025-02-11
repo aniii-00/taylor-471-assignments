68b6d9723b7fb74a66f4b54ef3a34c781e1c8f79
aniii-00 <anyssataylor@icloud.com>
2025-02-11 12:24:07 -0500

    first person pov

---
+++ 471-assignments/Assets/First Person/FirstPersonController.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class FirstPersonController : MonoBehaviour
+{
+//  -{ Variables}-    
+    Vector2 movement;
+    Vector2 mouseMovement;
+    float cameraUpRotation = 0.0f;
+    CharacterController controller;
+    [SerializeField]
+    float speed = 2.0f;
+    [SerializeField]
+    float mouseSensitivity = 100;
+
+    [SerializeField]
+    GameObject BulletSpawner;
+
+    [SerializeField]
+    GameObject cam;
+
+    
+    [SerializeField]
+    GameObject Bullet;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = movement.x;
+        float moveZ = movement.y;
+        float lookX= mouseMovement.x * Time.deltaTime * mouseSensitivity;
+        float lookY= mouseMovement.y * Time.deltaTime * mouseSensitivity;
+
+        cameraUpRotation -= lookY;
+
+        cameraUpRotation = Mathf.Clamp(cameraUpRotation , -90, 90);
+
+        cam.transform.localRotation = Quaternion.Euler(cameraUpRotation,0,0);
+
+        transform.Rotate(Vector3.up * lookX);
+
+        Vector3 actual_movement = (transform.forward * moveZ) + (transform.right * moveX);
+        controller.Move(actual_movement * Time.deltaTime * speed);
+    }
+
+    void OnMove(InputValue moveVal)
+    {
+        movement = moveVal.Get<Vector2>();
+    }
+
+    void OnLook(InputValue lookVal)
+    {
+        mouseMovement = lookVal.Get<Vector2>();
+    }
+
+    void OnAttack()
+    {
+        Instantiate(Bullet, BulletSpawner.transform.position, BulletSpawner.transform.rotation);
+    }
+}
